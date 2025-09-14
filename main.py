c6066f6d85b7f2bbd06003af7a140b540621516f
timothee.demares <timothee.demares@gmail.com>
2025-09-14 17:00:33 +0200

    Trouver une propriété utile de

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QLabel
 
 app = QApplication(sys.argv)
 
-label = QLabel("Hello, PyQt6!")
+label = QLabel()
+label.setTextFormat(Qt.TextFormat.RichText)
+label.setText("<b>Hello</b>, <i>PyQt6</i>!")
 
 with open("style.qss", "r", encoding="utf-8") as file:
     qss_style = file.read()

cbfeb22a3de5fbde3df6b5869a75a0f95030b334
timothee.demares <timothee.demares@gmail.com>
2025-09-14 17:44:28 +0200

    Structure minimale

---
+++ src/main_window.py
@@ -0,0 +1,36 @@
+from PyQt6.QtWidgets import QLineEdit, QMainWindow, QPushButton, QTextEdit, QVBoxLayout, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Ma Première Application, TP chapitre 1")
+        self.setGeometry(100, 100, 800, 600)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
+        line_edit = QLineEdit()
+        line_edit.textChanged.connect(self.on_line_edit_changed)
+        layout.addWidget(line_edit)
+
+        button = QPushButton("Click me")
+        button.clicked.connect(self.on_button_clicked)
+        layout.addWidget(button)
+
+        self.text_edit = QTextEdit()
+        self.text_edit.textChanged.connect(self.on_text_edit_changed)
+        layout.addWidget(self.text_edit)
+
+    def on_line_edit_changed(self, text: str) -> None:
+        print(f"Line edit changed: {text}")
+
+    def on_button_clicked(self) -> None:
+        print("Button clicked")
+
+    def on_text_edit_changed(self) -> None:
+        text = self.text_edit.toPlainText()
+        print(f"Text edit changed: {text}")

6cb4796fce6321453ac335418514c537ab3a245d
timothee.demares <timothee.demares@gmail.com>
2025-09-14 14:39:10 +0200

    add .qss for label

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from PyQt6.QtWidgets import QApplication, QLabel
 app = QApplication(sys.argv)
 
 label = QLabel("Hello, PyQt6!")
+
+with open("style.qss", "r", encoding="utf-8") as file:
+    qss_style = file.read()
+    app.setStyleSheet(qss_style)
+
 label.show()
 
 sys.exit(app.exec())

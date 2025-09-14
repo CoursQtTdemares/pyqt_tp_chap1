5d385643a1b5a184cbe5ecd7535fd0c507fce3ba
timothee.demares <timothee.demares@gmail.com>
2025-09-14 17:11:09 +0200

    Trouver un signal d'intérêt sur un bouton

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QLabel
+from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+
+
+def on_button_clicked() -> None:
+    print("Button clicked")
+
+
+def on_checkable_button_clicked() -> None:
+    global checkable_button
+    text = "Coche" if checkable_button.isChecked() else "Décoche"
+    checkable_button.setText(text)
+    message = "Checkable button clicked" if checkable_button.isChecked() else "Checkable button unchecked"
+    print(message)
+
 
 app = QApplication(sys.argv)
+
+
+main_widget = QWidget()
+layout = QVBoxLayout()
+
+main_widget.setLayout(layout)
 
 label = QLabel()
 label.setTextFormat(Qt.TextFormat.RichText)
 label.setText("<b>Hello</b>, <i>PyQt6</i>!")
+layout.addWidget(label)
+
+button = QPushButton("Click me")
+button.clicked.connect(on_button_clicked)
+layout.addWidget(button)
+
+checkable_button = QPushButton("Checkable")
+checkable_button.setCheckable(True)
+checkable_button.toggled.connect(on_checkable_button_clicked)
+layout.addWidget(checkable_button)
+
 
 with open("style.qss", "r", encoding="utf-8") as file:
     qss_style = file.read()
     app.setStyleSheet(qss_style)
 
-label.show()
+main_widget.show()
 
 sys.exit(app.exec())
